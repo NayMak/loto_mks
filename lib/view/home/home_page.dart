56b343f9906294d280f960efd63b867cc56ff2e3
ESTEDE TADDAY MAKUSA <20015733@M300041.hq.corp.leroymerlin.com>
2024-12-12 17:46:04 +0100

    add return bottom and SingleScrollPage

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -18,74 +18,152 @@ class HomePage extends StatelessWidget {
         value: LotoProvider(),
         child: Consumer<LotoProvider>(
           builder: (BuildContext context, provider, Widget? child) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'JEU DU BINGO !',
-                  style: TextStyle(
-                    fontSize: 96,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2.0, 2.0),
-                        blurRadius: 3.0,
-                        color: Colors.black.withValues(red: 0.5),
-                      ),
-                    ],
-                  ),
-                ),
-                Divider(
-                  color: Colors.grey.withValues(alpha: 0.3),
-                  thickness: 2,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Expanded(
-                          child: CircleButton(
-                            icon: FontAwesomeIcons.circlePlay,
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => LotoPage(),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Expanded(
-                          child: Text(
-                            'Jeu réalisé par Émile Makusa et Cedric Willem',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey,
-                              shadows: [
-                                Shadow(
-                                  offset: Offset(1, 1),
-                                  blurRadius: 1.0,
-                                  color: Colors.black.withValues(alpha: 0.5),
-                                ),
-                              ],
-                            ),
-                          ),
+            return Center(
+              child: Column(
+                children: [
+                  Text(
+                    'JEU DU BINGO !',
+                    style: TextStyle(
+                      fontSize: 96,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 3.0,
+                          color: Colors.black.withValues(red: 0.5),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ],
+                  Divider(
+                    color: Colors.grey.withValues(alpha: 0.3),
+                    thickness: 2,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          spacing: 16,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2,
+                                ),
+                              ),
+                              child: Column(
+                                spacing: 8,
+                                children: [
+                                  Text(
+                                    'Règle du jeu :',
+                                    style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey,
+                                      shadows: [
+                                        Shadow(
+                                          offset: Offset(1, 1),
+                                          blurRadius: 1.0,
+                                          color:
+                                              Colors.black.withValues(red: 0.3),
+                                        ),
+                                      ],
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    "L'objectif du jeu est d'être le premier à obtenir tous les numéros jusqu'à remplir le carton entier. Lorsque le carton est rempli, crier :",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey,
+                                      shadows: [
+                                        Shadow(
+                                          offset: Offset(1, 1),
+                                          blurRadius: 1.0,
+                                          color: Colors.black
+                                              .withValues(alpha: 0.5),
+                                        ),
+                                      ],
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    'Bingo !',
+                                    style: TextStyle(
+                                      fontSize: 58,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey,
+                                      shadows: [
+                                        Shadow(
+                                          offset: Offset(1, 1),
+                                          blurRadius: 1.0,
+                                          color:
+                                              Colors.black.withValues(red: 0.3),
+                                        ),
+                                      ],
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              'Exemple de carton :',
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                                shadows: [
+                                  Shadow(
+                                    offset: Offset(1, 1),
+                                    blurRadius: 1.0,
+                                    color: Colors.black.withValues(alpha: 0.5),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Image.asset(
+                              'assets/grids/grid-exemple.png',
+                            ),
+                            CircleButton(
+                              icon: FontAwesomeIcons.circlePlay,
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => LotoPage(),
+                                  ),
+                                );
+                              },
+                            ),
+                            Text(
+                              'Jeu réalisé par Émile Makusa et Cedric Willem',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                                shadows: [
+                                  Shadow(
+                                    offset: Offset(1, 1),
+                                    blurRadius: 1.0,
+                                    color: Colors.black.withOpacity(0.5),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             );
           },
         ),
--- lib/view/loto/loto_page.dart
+++ lib/view/loto/loto_page.dart
@@ -20,159 +20,172 @@ class _LotoPageState extends State<LotoPage> {
         value: LotoProvider(),
         child: Consumer<LotoProvider>(
           builder: (BuildContext context, provider, Widget? child) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'JEU DU BINGO !',
-                  style: TextStyle(
-                    fontSize: 96,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2.0, 2.0),
-                        blurRadius: 3.0,
-                        color: Colors.black.withValues(red: 0.5),
-                      ),
-                    ],
-                  ),
-                ),
-                Divider(
-                  color: Colors.grey.withValues(alpha: 0.3),
-                  thickness: 2,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2,
-                              ),
-                            ),
-                            child: provider.currentCard == null
-                                ? Center(
-                                    child: const Text(
-                                      'Lancer le jeu et tirer une carte pour commencer',
-                                    ),
-                                  )
-                                : Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Center(
-                                      child: Image(
-                                        image: AssetImage(
-                                          provider.currentCard!.asset,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                          ),
-                        ),
-                        SizedBox(width: 16),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Visibility(
-                              visible: !provider.isGameStarted,
-                              child: CircleButton(
-                                icon: FontAwesomeIcons.circlePlay,
-                                onPressed: () {
-                                  provider
-                                    ..init()
-                                    ..startGame();
-                                },
-                              ),
-                            ),
-                            Visibility(
-                              visible: provider.isGameStarted,
-                              child: CircleButton(
-                                icon: FontAwesomeIcons.fileArrowUp,
-                                onPressed: () {
-                                  provider.selectRandomCard();
-                                },
-                              ),
-                            ),
-                            SizedBox(height: 16),
-                            Visibility(
-                              visible: provider.isGameStarted,
-                              child: CircleButton(
-                                icon: FontAwesomeIcons.arrowRotateRight,
-                                onPressed: () {
-                                  provider
-                                    ..init()
-                                    ..startGame();
-                                },
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(width: 16),
-                        Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.all(8.0),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2,
-                              ),
-                            ),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    CircleButton(
-                                      icon: FontAwesomeIcons.tableCellsLarge,
-                                      onPressed: () {
-                                        provider.updateGridViewAxisCount(3);
-                                      },
-                                      disable: provider.gridViewAxisCount == 3,
-                                    ),
-                                    SizedBox(width: 16),
-                                    CircleButton(
-                                      icon: FontAwesomeIcons.tableCells,
-                                      onPressed: () {
-                                        provider.updateGridViewAxisCount(5);
-                                      },
-                                      disable: provider.gridViewAxisCount == 5,
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(height: 16),
-                                Expanded(
-                                  child: GridView.count(
-                                    primary: false,
-                                    padding: const EdgeInsets.all(8),
-                                    crossAxisSpacing: 4,
-                                    mainAxisSpacing: 4,
-                                    crossAxisCount: provider.gridViewAxisCount,
-                                    children: [
-                                      ...provider.announcedCards.map((card) {
-                                        return Image(
-                                          image: AssetImage(card.asset),
-                                        );
-                                      }),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+            return Center(
+              child: Column(
+                children: [
+                  Text(
+                    'JEU DU BINGO !',
+                    style: TextStyle(
+                      fontSize: 96,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 3.0,
+                          color: Colors.black.withValues(red: 0.5),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ],
+                  Divider(
+                    color: Colors.grey.withValues(alpha: 0.3),
+                    thickness: 2,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        spacing: 16,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2,
+                                ),
+                              ),
+                              child: provider.currentCard == null
+                                  ? Center(
+                                      child: const Text(
+                                        'Lancer le jeu et tirer une carte pour commencer',
+                                      ),
+                                    )
+                                  : Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Center(
+                                        child: Image(
+                                          image: AssetImage(
+                                            provider.currentCard!.asset,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Visibility(
+                                visible: !provider.isGameStarted,
+                                child: CircleButton(
+                                  icon: FontAwesomeIcons.circlePlay,
+                                  onPressed: () {
+                                    provider
+                                      ..init()
+                                      ..startGame();
+                                  },
+                                ),
+                              ),
+                              Visibility(
+                                visible: provider.isGameStarted,
+                                child: CircleButton(
+                                  icon: FontAwesomeIcons.fileArrowUp,
+                                  onPressed: () {
+                                    provider.selectRandomCard();
+                                  },
+                                ),
+                              ),
+                              SizedBox(height: 16),
+                              Visibility(
+                                visible: provider.isGameStarted,
+                                child: CircleButton(
+                                  icon: FontAwesomeIcons.arrowRotateRight,
+                                  onPressed: () {
+                                    provider
+                                      ..init()
+                                      ..startGame()
+                                      ..isGameStarted = false;
+                                  },
+                                ),
+                              ),
+                              Visibility(
+                                visible: !provider.isGameStarted,
+                                child: CircleButton(
+                                  icon: FontAwesomeIcons.arrowLeft,
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: const EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2,
+                                ),
+                              ),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      CircleButton(
+                                        icon: FontAwesomeIcons.tableCellsLarge,
+                                        onPressed: () {
+                                          provider.updateGridViewAxisCount(3);
+                                        },
+                                        disable:
+                                            provider.gridViewAxisCount == 3,
+                                      ),
+                                      SizedBox(width: 16),
+                                      CircleButton(
+                                        icon: FontAwesomeIcons.tableCells,
+                                        onPressed: () {
+                                          provider.updateGridViewAxisCount(5);
+                                        },
+                                        disable:
+                                            provider.gridViewAxisCount == 5,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 16),
+                                  Expanded(
+                                    child: GridView.count(
+                                      primary: false,
+                                      padding: const EdgeInsets.all(8),
+                                      crossAxisSpacing: 4,
+                                      mainAxisSpacing: 4,
+                                      crossAxisCount:
+                                          provider.gridViewAxisCount,
+                                      children: [
+                                        ...provider.announcedCards.map((card) {
+                                          return Image(
+                                            image: AssetImage(card.asset),
+                                          );
+                                        }),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
           },
         ),
